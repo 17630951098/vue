9cc0d23b53f35573b808ad73da774c02c25c31ef
hdz <1504177340@qq.com>
2020-04-30 14:12:08 +0800

    all

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -22,21 +22,13 @@
                     <div class="sc check" :style="{ borderColor: telBot ? '#000' : '' }">
                         <i class="phone"></i>
                         <input type="hidden" name="type"/>
-                        <input
-                                v-model="checkNum"
-                                type="text"
-                                placeholder="请输入图形验证码"
-                        />
-                        <b @click="getNewNum">{{osg}}</b>
+                        <input v-model="checkNum" type="text" placeholder="请输入图形验证码"/>
+                        <b @click="getNewNum">{{ osg }}</b>
                     </div>
                     <div class="sc check" :style="{ borderColor: telBot ? '#000' : '' }">
                         <i class="phone"></i>
                         <input type="hidden" name="type"/>
-                        <input
-                                v-model="telNum"
-                                type="text"
-                                placeholder="请输入手机验证码"
-                        />
+                        <input v-model="telNum" type="text" placeholder="请输入手机验证码"/>
                         <span @click="checkout">发送验证码</span>
                     </div>
                     <div class="sc check" :style="{ borderColor: telBot ? '#000' : '' }">
@@ -50,7 +42,9 @@
                         />
                     </div>
                     <p>用户注册代表同 <a href="#">《服务条款》</a>级<a href="#">《隐私条款》</a></p>
-                    <button :style="{backgroundColor:canRegister?'#1bbc9b':''}" @click="register">立即注册</button>
+                    <button :style="{ backgroundColor: canRegister ? '#1bbc9b' : '' }" @click="register">
+                        立即注册
+                    </button>
                 </form>
             </div>
         </div>
@@ -68,56 +62,61 @@
                 showDel: false,
                 tel: "",
                 //图形验证码
-                checkNum:'',
+                checkNum: "",
                 //获取到的图形验证码
-                password:'',
-                osg:'',
-                telNum:'',
-                canRegister:false,
+                password: "",
+                osg: "",
+                telNum: "",
+                canRegister: false,
             };
         },
         methods: {
-            cahangeSub(){
-                if (this.tel!=''&&this.password!=''&&this.checkNum!=''&&this.telNum!=''){
-                    this.canRegister=true
-                }else {
-                    this.canRegister = false
+            cahangeSub() {
+                if (this.tel != "" && this.password != "" && this.checkNum != "" && this.telNum != "") {
+                    this.canRegister = true;
+                } else {
+                    this.canRegister = false;
                 }
             },
-            register(){
-               if (!/^[A-z0-9-_]{6,20}$/.test(this.password)){
-                    this.$toast('请输入合法密码');
-                    this.getNewNum()
-                }else {
-                    this.$toast.success('注册成功,2s后跳转登录页面');
-                    this.$router.push('/login')
+            register() {
+                if (!/^[A-z0-9-_]{6,20}$/.test(this.password)) {
+                    this.$toast("请输入合法密码");
+                    this.getNewNum();
+                } else {
+                    this.$toast.success("注册成功,2s后跳转登录页面");
+                    this.$router.push("/login");
                 }
             },
-            getNewNum(){
-                this.$api.getImgAPI().then(res => {
-                    // console.log()
-                    this.osg = res.data
-                }).catch(err => {
-                    this.$toast('网络错误')
-                })
-            },
-            checkout(){
-                if (this.checkNum==''||this.tel==''){
-                    this.$toast('请输入手机号或图形验证码')
-                }else if (!/^\d{11}$/.test(this.tel)){
-                    this.$toast('请输入11位手机号')
-                }
-                else if(this.checkNum!=this.osg){
-                    this.$toast('图形验证码错误')
-                }else {
-                    this.$toast('验证码已发送');
-                    this.$api.getTelAPI({
-                        tel:this.tel
-                    }).then(res=>{
-                        // this.$toast('验证码为'+res.data);
-                    }).catch(err=>{
-                        this.$toast('网络错误');
+            getNewNum() {
+                this.$api
+                    .getImgAPI()
+                    .then((res) => {
+                        // console.log()
+                        this.osg = res.data;
                     })
+                    .catch((err) => {
+                        this.$toast("网络错误");
+                    });
+            },
+            checkout() {
+                if (this.checkNum == "" || this.tel == "") {
+                    this.$toast("请输入手机号或图形验证码");
+                } else if (!/^\d{11}$/.test(this.tel)) {
+                    this.$toast("请输入11位手机号");
+                } else if (this.checkNum != this.osg) {
+                    this.$toast("图形验证码错误");
+                } else {
+                    this.$toast("验证码已发送");
+                    this.$api
+                        .getTelAPI({
+                            tel: this.tel,
+                        })
+                        .then((res) => {
+                            // this.$toast('验证码为'+res.data);
+                        })
+                        .catch((err) => {
+                            this.$toast("网络错误");
+                        });
                 }
             },
             del(type) {
@@ -131,12 +130,15 @@
             },
         },
         created() {
-            this.$api.getImgAPI().then(res=>{
-                // console.log()
-                this.osg= res.data
-            }).catch(err=>{
-                this.$toast('网络错误')
-            })
+            this.$api
+                .getImgAPI()
+                .then((res) => {
+                    // console.log()
+                    this.osg = res.data;
+                })
+                .catch((err) => {
+                    this.$toast("网络错误");
+                });
         },
         mounted() {
             this.$nextTick(function () {
@@ -211,7 +213,7 @@
                     text-decoration: none;
                     display: block;
                 }
-    
+                
                 input[type="password"] {
                     flex: 1;
                     margin-left: 0.16rem;
@@ -226,29 +228,32 @@
                     display: block;
                 }
             }
-            .sc:nth-of-type(2){
-                i{
+            
+            .sc:nth-of-type(2) {
+                i {
                     background-size: 1.68rem 1.68rem;
-                    width: .42rem;
-                    height: .42rem;
-                    background-position: -.82rem 0;
+                    width: 0.42rem;
+                    height: 0.42rem;
+                    background-position: -0.82rem 0;
                 }
             }
-            .sc:nth-of-type(3){
-                i{
+            
+            .sc:nth-of-type(3) {
+                i {
                     background-size: 1.68rem 1.68rem;
-                    width: .42rem;
-                    height: .42rem;
+                    width: 0.42rem;
+                    height: 0.42rem;
                     background-position: -1.26rem 0;
                     background-repeat: no-repeat;
                 }
-                span{
+                
+                span {
                     width: 2rem;
-                    height: .68rem;
-                    line-height: .68rem;
+                    height: 0.68rem;
+                    line-height: 0.68rem;
                     display: inline-block;
-                    font-size: .24rem;
-                    border-radius: .34rem;
+                    font-size: 0.24rem;
+                    border-radius: 0.34rem;
                     background-color: #1bbc9b;
                     border: none;
                     text-align: center;
@@ -256,34 +261,38 @@
                     outline: none;
                 }
             }
-            .sc:nth-of-type(4){
-                input{
+            
+            .sc:nth-of-type(4) {
+                input {
                 }
             }
-            p{
+            
+            p {
                 align-items: center;
-                padding: .4rem 0 0;
-                font-size: .24rem;
+                padding: 0.4rem 0 0;
+                font-size: 0.24rem;
                 color: #999;
-                a{
+                
+                a {
                     color: turquoise;
                 }
             }
-            button{
+            
+            button {
                 margin-top: 0.3rem;
-                margin-bottom: .3rem;
-                border-radius: .03rem;
+                margin-bottom: 0.3rem;
+                border-radius: 0.03rem;
                 overflow: hidden;
                 position: relative;
                 z-index: 123;
-                height: .88rem;
-                line-height: .8rem;
+                height: 0.88rem;
+                line-height: 0.8rem;
                 display: block;
                 width: 6.3rem;
-                font-size: .28rem;
+                font-size: 0.28rem;
                 background: #ccc;
                 border: 0;
-                color: #FFFFFF;
+                color: #ffffff;
             }
         }
     }
